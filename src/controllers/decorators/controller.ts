48bef53a8d0ed03d242730df721e8d954fb80b3c
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2021-10-06 23:56:52 -0300

    implement middleware in the route definitiion

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -9,10 +9,23 @@ export function controller(routePrefix: string) {
     const router = AppRouter.getInstance()
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key]
-      const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key)
-      const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key)
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      )
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      )
+      const middlewares = Reflect.getMetadata(
+        MetadataKeys.middleware,
+        target,
+        key
+      ) || []
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler)
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler)
       }
     }
   }

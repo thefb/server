d2c9acc98c0d6c38c72de056da31d589d2f07c14
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2021-10-06 23:45:52 -0300

    MetadataKeys Enums

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata'
 import { AppRouter } from '../../AppRouter'
+import { MetadataKeys } from './MetadataKeys'
 import { Methods } from './Methods'
+
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance()
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key]
-      const path = Reflect.getMetadata('path', target.prototype, key)
-      const method: Methods = Reflect.getMetadata('method', target.prototype, key)
+      const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key)
+      const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key)
       if (path) {
         router[method](`${routePrefix}${path}`, routeHandler)
       }
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata'
+import { MetadataKeys } from './MetadataKeys'
 
 function routerBinder(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key)
-      Reflect.defineMetadata('method', method, target, key)
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key)
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key)
     }
   }
 }
